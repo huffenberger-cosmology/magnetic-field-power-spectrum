37c7920c90f910dcd29cc968c0ed28dd68c8fd60
huffenbe <khuffenberger@fsu.edu>
2019-10-15 10:25:25 -0400

    Had the python module code in the wrong .c file.

--- code/Bpowspec.c
+++ code/Bpowspec.c
@@ -1,14 +1,2 @@
-#include <Python.h>
-#include <numpy/ndarrayobject.h>
-#include <gsl/gsl_spline.h>
-#include <gsl/gsl_rng.h>
-
-static PyMethodDef BpowspecMethods[] = {
-  {NULL, NULL, 0, NULL}        /* Sentinel */
-};
-
-PyMODINIT_FUNC initBpowspec(void)
-{
-  (void) Py_InitModule("Bpowspec", BpowspecMethods);
-  import_array();  // This is important for using the numpy_array api, otherwise segfaults!
-}
+#include <stdio.h>
+#include <stdlib.h>
--- code/Bpowspec_mod.c
+++ code/Bpowspec_mod.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <Python.h>
+#include <numpy/ndarrayobject.h>
+#include <gsl/gsl_spline.h>
+#include <gsl/gsl_rng.h>
+
+static PyMethodDef BpowspecMethods[] = {
+  {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+PyMODINIT_FUNC initBpowspec(void)
+{
+  (void) Py_InitModule("Bpowspec", BpowspecMethods);
+  import_array();  // This is important for using the numpy_array api, otherwise segfaults!
+}
